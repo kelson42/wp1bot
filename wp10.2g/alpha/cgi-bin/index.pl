78f530cd8d088b3008b8ecbf7116804637cca79f
titoxd <titoxd@9851563e-e8c4-40cd-968a-2c98415fa2d2>
2008-08-22 06:02:44 +0000

    * added progress bar to index * NOTE: it is a really ugly hack

--- wp10.2g/alpha/cgi-bin/index.pl
+++ wp10.2g/alpha/cgi-bin/index.pl
@@ -13,6 +13,7 @@ our $Opts = read_conf();
 require 'database_www.pl';
 require 'layout.pl';
 require 'init_cache.pl';
+require Mediawiki::API;
 
 require CGI;
 require CGI::Carp; 
@@ -48,7 +49,6 @@ exit;
 
 #####################################################################
 
-
 sub project_index_link { 
   my $project = shift;
   my $data = shift;
@@ -74,6 +74,32 @@ sub project_index_link {
            . uri_escape($data->{'p_wikipage'}) . "\">homepage</a>";
   }
 
+	if ( $data->{'p_count'} != 0 ) { 
+		print_progress_bar(($data->{'p_qcount'} / $data->{'p_count'}) * 100);
+		print_progress_bar(($data->{'p_icount'} / $data->{'p_count'}) * 100);
+	}
+	
   $line .= "</li>\n";
   print $line;
+}
+
+#####################################################################
+# FIXME: hack hack hack 
+# Instead of making the code resemble frwiki's output, we just
+# copy it from frwiki via the API
+# TODO: figure out how the {{avancement}} template works, and
+# copy it to enwiki
+sub print_progress_bar {
+	my $number = shift;
+	my $fr_api = new Mediawiki::API;
+	$fr_api->debug_level(0); # no output at all 
+	$fr_api->base_url('http://fr.wikipedia.org/w/api.php');
+	
+	# Format the input to two decimal digits
+	my $rounded = sprintf("%.2f", $number);
+	my $r =  $fr_api->parse("{{Avancement|avancement=$rounded}}");
+	my $t = $r->{'text'};
+	$t =~ s!^<p>!!;
+	my @t = split('</p>',$t);
+	print $t;
 }
