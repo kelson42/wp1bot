053099966fc9440a4fc20f8cc761821d300aa84e
cbm <cbm@9851563e-e8c4-40cd-968a-2c98415fa2d2>
2008-08-24 16:45:33 +0000

    Change read_conf.pl to use the Perl parser for the file.
    Update example file to match. 
    
    NOTE: this breaks old configuration files

--- wp10.2g/alpha/read_conf.pl
+++ wp10.2g/alpha/read_conf.pl
@@ -1,13 +1,13 @@
 #!/usr/bin/perl
 use strict;
 use Data::Dumper;
-# Internal variable holding the configuration variables
-my $settings = {};
+
+my $Settings = read_conf();
 
 sub read_conf { 
-  print "Reading configuration file\n";
   my $filename;
-  my $homedir = (getpwuid($<)) [7];
+  my $settings;
+  my $homedir = (getpwuid($<))[7];
   
   if ( defined $ENV{'WP10_CREDENTIALS'} ) {
     $filename = $ENV{'WP10_CREDENTIALS'};
@@ -15,92 +15,53 @@ sub read_conf {
     $filename = $homedir . "/.wp10.conf";
   }
 
-  die "Can't open database configuration '$filename'\n"
+  die "Can't open configuration file '$filename'\n"
     unless -r $filename;
 
-  my ($opt, $val, $line);
-  my $opts = {};
+  print "Reading configuration file '$filename'\n";
 
-  open CONF, "<", $filename;
+  open CONF, "<", $filename 
+    or die "Can't open configuration file '$filename': $!\n";
+
+  my $text = "";
+  my $line;
+  
   while ( $line = <CONF> ) {
-    next if ( $line =~ /\s*#/ );
-    next if ( $line =~ /^\s*$/ );
-    chomp $line;
-    ($opt, $val) = split /\s+/, $line, 2;
-    $val =~ s/\s*$//;
-
-    if ( $opt eq 'lib' ) { 
-      push @INC, $val;
-    } else { 
-      $opts->{$opt} = $val;
-	  $settings->{$opt} = $val;
-    }
+    $text .= $line;
   }
   close CONF;
 
-  return $opts;
+  $settings = eval '{ ' . $text . ' }';
+
+  if ( $@ ) { 
+    die "\nError parsing configuration file '$filename':\n  $@\n";
+  }
+
+
+  foreach $line ( @{$settings->{'lib'}} ) {
+    push @INC, $line;
+  }
+
+
+  local $Data::Dumper::Terse = 1;
+  local $Data::Dumper::Sortkeys = 1;
+  print "Configuration settings: \n";
+  print Dumper($settings);
+
+  print "Include path (\@INC):\n\t";
+  print (join "\n\t", @INC) . "\n";
+
+
+
+  return $settings;
+
 }
 
-sub get_conf() { 
-	my $var = shift;
-	my ($hash, $val, $key, $left, $right);
-	
-	if ( !defined $var)
-	{
-		print "Missing \$var parameter\n";
-		die();
-	}
-	
-	
-	if ( defined $settings->{$var})
-	{
-		$val = $settings->{$var};
-		# print "$var found in settings; value = $val\n";
-		if ($val =~ /^\{/ && $val =~ /\}$/)
-		{
-			# Assume that this is a hash variable
-			
-			# Remove the { and } that are wrapping the hash
-			$val = substr($val, 1, length($val) - 2);
-			
-			my $vars;
-			
-			# Split along the commas
-			my @keys = split(/,/, $val);
-			
-			# Loop around each key/value pair
-			foreach $key (@keys) {
-				# Parse around the '=>'
-				next unless ( $key =~ /^\s*(\S*)\s*\=>\s*(\S*)\s*$/ ); 
-				$left = $1;
-				$right = $2;
-				# Remove the quotes surrounding key/value pairs, if necessary
-				if ($left =~ /^\'/ && $left =~ /\'$/)
-				{
-					$left = substr($left, 1, length($left) - 2);
-				}
-				if ( $right =~ /^\'/ && $right =~ /\'$/)
-				{
-					$right = substr($right, 1, length($right) - 2);
-				}
-				# Assign the key/value pair to the associative array hash
-				$vars->{$left} = $right;   
-			}
-			return $vars;
-		}
-		else
-		{
-			# Not a hash variable
-			return $val;
-		}
-	}
-	else
-	{
-		print Dumper($var);
-		print "$var not found in settings\n";
-		die();
-	}
+sub get_conf { 
+  my $var = shift;
+  return $Settings->{$var};
 }
+
 
 
 # Load successfully
