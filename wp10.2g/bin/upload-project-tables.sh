df5030bbe041365681fb351b78179a2e9fbe45c3
cbm <cbm@9851563e-e8c4-40cd-968a-2c98415fa2d2>
2010-02-13 19:39:33 +0000

    Debugging output for update script

--- wp10.2g/bin/upload-project-tables.sh
+++ wp10.2g/bin/upload-project-tables.sh
@@ -8,6 +8,11 @@ cd $PDIR/backend
 
 pwd
 
+echo "@@ copy_tables --project"
 time ./copy_tables.pl --project
+echo "@@ copy_tables --global"
 time ./copy_tables.pl --global
+echo "@@ copy_count"
 time ./copy_count.pl --global
+echo "@@ copy_logs"
+time ./copy_logs.pl --all
