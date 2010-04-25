bb2ba4affb68afadec75a5f9353f00eac3e773ae
cbm <cbm@9851563e-e8c4-40cd-968a-2c98415fa2d2>
2010-04-25 18:51:28 +0000

    Add custom table for US roads project

--- wp10.2g/backend/custom_tables.pl
+++ wp10.2g/backend/custom_tables.pl
@@ -73,5 +73,181 @@ HERE
 
 }
 
+sub custom_roads_table_1 {
+
+  my $RoadProjectData = [
+    'IH' => 'Interstate Highway System',
+    'USH' => 'U.S. Highway system',
+    'Auto trail' => 'U.S. auto trail',
+    'Alabama' => 'Alabama road transport',
+    'Alaska' => 'Alaska road transport',
+    'Arizona' => 'Arizona road transport',
+    'Arkansas' => 'Arkansas road transport',
+    'California' => 'California road transport',
+    'Colorado' => 'Colorado road transport',
+    'Connecticut' => 'Connecticut road transport',
+    'Delaware' => 'Delaware road transport',
+    'D.C.' => 'District of Columbia road transport',
+    'Florida'=> 'Florida road transport',
+    'Georgia' => 'Georgia (U.S. state) road transport',
+    'Guam' => 'Guam road transport',
+    'Hawaii' => 'Hawaii road transport',
+    'Idaho' => 'Idaho road transport',
+    'Illinois' => 'Illinois road transport',
+    'Indiana' => 'Indiana road transport',
+    'Iowa' => 'Iowa road transport',
+    'Kansas' => 'Kansas road transport',
+    'Kentucky' => 'Kentucky road transport',
+    'Louisiana' => 'Louisiana road transport',
+    'Maine' => 'Maine road transport',
+    'Maryland' => 'Maryland road transport',
+    'Massachusetts' => 'Massachusetts road transport',
+    'Michigan' => 'Michigan road transport',
+    'Minnesota' => 'Minnesota road transport',
+    'Mississippi' => 'Mississippi road transport',
+    'Missouri' => 'Missouri road transport',
+    'Montana' => 'Montana road transport',
+    'Nebraska' => 'Nebraska road transport',
+    'Nevada' => 'Nevada road transport',
+    'New Hampshire' => 'New Hampshire road transport',
+    'New Jersey' => 'New Jersey road transport',
+    'New Mexico' => 'New Mexico road transport',
+    'New York' => 'New York road transport',
+    'North Carolina' => 'North Carolina road transport',
+    'North Dakota' => 'North Dakota road transport',
+    'Ohio' => 'Ohio road transport',
+    'Oklahoma' => 'Oklahoma road transport',
+    'Oregon' => 'Oregon road transport',
+    'Pennsylvania' => 'Pennsylvania road transport',
+    'Puerto Rico' => 'Puerto Rico road transport',
+    'Rhode Island' => 'Rhode Island road transport',
+    'South Carolina' => 'South Carolina road transport',
+    'South Dakota' => 'South Dakota road transport',
+    'Tennessee' => 'Tennessee road transport',
+    'Texas' => 'Texas road transport',
+    'U.S. Virgin Islands' => 'U.S. Virgin Islands road transport',
+    'Utah' => 'Utah road transport',
+    'Vermont' => 'Vermont road transport',
+    'Virginia' => 'Virginia road transport',
+    'Washington' => 'Washington road transport',
+    'West Virginia' => 'West Virginia road transport',
+    'Wisconsin' => 'Wisconsin road transport',
+    'Wyoming' => 'Wyoming road transport',
+    'USRD' => 'U.S. road transport'
+  ];
+
+  my $RoadProjectsGrey = {
+    'IH' => 1
+  };
+   
+  my $i;
+  my $RoadProjects = [];
+  my $RoadProjectCats = {};
+  for ( $i = 0; $i < scalar @$RoadProjectData; $i+= 2) { 
+    push @$RoadProjects, $RoadProjectData->[$i];
+    $RoadProjectCats->{$RoadProjectData->[$i]} 
+                         = $RoadProjectData->[$i+1];
+  }
+
+  my $Classes = {
+    'FA-Class' =>    {'sort'=>1, 'weight'=>0, 'name'=>'FA'},
+    'A-Class' =>     {'sort'=>2, 'weight'=>1, 'name'=>'A'},
+    'GA-Class' =>    {'sort'=>3, 'weight'=>2, 'name'=>'GA'},
+    'B-Class' =>     {'sort'=>4, 'weight'=>3, 'name'=>'B'},
+    'C-Class' =>     {'sort'=>5, 'weight'=>4, 'name'=>'C'},
+    'Start-Class' => {'sort'=>6, 'weight'=>5, 'name'=>'Start'},
+    'Stub-Class' =>  {'sort'=>7, 'weight'=>6, 'name'=>'Stub'},
+  };
+
+  my $dbh = database_handle();
+
+  my $sth = $dbh->prepare('select r_quality, count(r_article) as num 
+                             from ratings 
+                              where r_project  = ? 
+                              and r_namespace = 0 
+                              group by r_quality');
+
+  my ( $proj, $cat, $data, $class, $weight, $omega, $total, $num);
+
+  my $text = << "HERE";
+{|class="wikitable sortable"
+|-
+!State
+!{{FA-Class|category=Category:FA-Class U.S. road transport articles}}
+!{{A-Class|category=Category:A-Class U.S. road transport articles}}
+!{{GA-Class|category=Category:GA-Class U.S. road transport articles}}
+!{{B-Class|category=Category:B-Class U.S. road transport articles}}
+!{{C-Class|category=Category:C-Class U.S. road transport articles}}
+!{{Start-Class|category=Category:Start-Class U.S. road transport articles}}
+!{{Stub-Class|category=Category:Stub-Class U.S. road transport articles}}
+!&#969;
+!&#937;
+HERE
+
+$i = 0;
+
+  foreach $proj ( @$RoadProjects ) { 
+    $omega = 0;
+    $total = 0;
+
+    $text .= "|-\n";
+
+    if ( defined $RoadProjectsGrey->{$proj} ) { 
+      $text .= "!bgcolor=silver|";
+    } else { 
+      $text .= "!";
+    }
+
+    $text .= "[[Wikipedia:Version 1.0 Editorial Team/" 
+                 . $RoadProjectCats->{$proj}
+              . " articles by quality statistics|" . $proj . "]]\n";
+
+    $cat = $RoadProjectCats->{$proj};
+
+    $sth->execute($cat);
+    $data = $sth->fetchall_hashref('r_quality');
+
+    foreach $class ( sort { $Classes->{$a}->{'sort'} 
+                            <=> $Classes->{$b}->{'sort'} } 
+                     keys %$Classes ) { 
+      $weight = $Classes->{$class}->{'weight'};
+      if ( ! defined $data->{$class} ) { 
+        $num = 0;
+      } else { 
+        $num = $data->{$class}->{'num'};
+      }
+
+      if ( defined $RoadProjectsGrey->{$proj} ) { 
+        $text .= "|bgcolor=silver";
+      }  
+      $text .= "|$num\n";
+
+      $total += $num;
+      $omega += $num * $weight;
+   }
+
+   if ( defined $RoadProjectsGrey->{$proj} ) { 
+     $text .= "|bgcolor=silver";
+   }  
+   $text .= "|$omega\n";
+
+   if ( defined $RoadProjectsGrey->{$proj} ) { 
+     $text .= "|bgcolor=silver";
+   }  
+
+   if ( $total > 0 ) { 
+     $text .= "|" . (sprintf("%2.4f", $omega/ $total)) . "\n";
+   } else { 
+      $text .= "|&ndash;\n";   
+   }
+
+   $i++;
+ }
+
+  $text .= "|}";
+  return $text;
+
+}
+
 1;
 
