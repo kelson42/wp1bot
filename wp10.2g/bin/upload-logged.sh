bea566088b65264474e7143e59f91fbe79112d47
cbm <cbm@9851563e-e8c4-40cd-968a-2c98415fa2d2>
2011-04-04 21:40:20 +0000

    Better logging

--- wp10.2g/bin/upload-logged.sh
+++ wp10.2g/bin/upload-logged.sh
@@ -1,12 +1,19 @@
 #!/opt/ts/bin/bash
 
+LANG="en_US.UTF-8"
+export LANG
+
 PDIR=/home/project/e/n/w/enwp10
 
 FILE=$PDIR/Logs/upload.`date +%Y-%m-%d_%H:%M.txt`
 
 UPDATE=$PDIR/bin/upload-project-tables.sh
 
-$UPDATE &> $FILE
+/bin/date >> $FILE
+
+$UPDATE &>> $FILE
+
+/bin/date >> $FILE
 
 cd $PDIR
 gzip -9 $FILE
